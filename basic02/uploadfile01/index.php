60612ce28864dd7990cffef8440589ffde6668fa
phuhh <huynhhieu.phu@gmail.com>
2023-03-06 23:16:20 +0700

    fix small

--- basic02/uploadfile01/index.php
+++ basic02/uploadfile01/index.php
@@ -35,22 +35,22 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     if($file['error'] === 0) {
         //Validate file
         if(!in_array($extension, $allowExt)) {
-            $message['file']['ext_err'] = 'Định dạng tập tin không đúng';
+            exit('Định dạng tập tin không đúng');
         } else if ($fileSize > $allowSize) {
-            $message['file']['size_err'] = 'Kích thước tập tin tối đa 1MB';
+            exit('Kích thước tập tin tối đa 1MB');
         }
 
         // upload file
         if(empty($message)) {
             $uploads = @move_uploaded_file($fileTmp, 'uploads/' . $newName);
             if(!$uploads) {
-                $message['file']['move_err'] = 'Di chuyển tập tin bị lỗi';
+                exit('Di chuyển tập tin bị lỗi');
             }else {
-                $message['file']['update_success'] = 'Tải tập tin thành công';
+                exit('Tải tập tin thành công');
             }
         }
     } else {
-        $message['file']['upload_err'] = 'Tập tin tải lên lỗi';
+        exit('Tập tin tải lên lỗi');
     }
 
 }
@@ -59,19 +59,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <form method="post" enctype="multipart/form-data">
     <p>
         <input type="file" name="file"><br>
-        <?php
-        echo isset($message['file']['upload_err']) ? '<span style="color: red">'.$message['file']['upload_err'].'</span>' : false;
-        echo isset($message['file']['ext_err']) ? '<span style="color: red">'.$message['file']['ext_err'].'</span>' : false;
-        echo isset($message['file']['size_err']) ? '<span style="color: red">'.$message['file']['size_err'].'</span>' : false;
-        echo isset($message['file']['move_err']) ? '<span style="color: red">'.$message['file']['move_err'].'</span>' : false;
-        ?> 
-        
     </p>
     <p>
         <button type="submit">Submit</button>
-        <?php
-        echo isset($message['file']['update_success']) ? '<span style="color: green">'.$message['file']['update_success'].'</span>' : false;
-        ?>
     </p>
 </form>
 
